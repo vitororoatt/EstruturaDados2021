5aa1cd241094d78a3d3441d3bf3bf16f4c43dc57
marizmayara <mayaramariz@yahoo.com.br>
2021-03-07 20:53:38 -0300

    Atualizações
    Deixei o cód clean

--- hello_world.c
+++ hello_world.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
-    printf("ola world");
+
+    printf("Roi world");
     int a = 10;
     int b = 20;
     int c;
     c = a+b;
     printf("A soma de A + B eh: %d", c);
-=======
-    printf("Roi world");
->>>>>>> 1ba882a1e9a92402985c9f1d1ce5bdf0fd95554e
+
     return 0;
 }
