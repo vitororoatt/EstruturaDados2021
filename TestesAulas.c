771a50dab8a0afb5f4b86ddfe890ebab9f75a7c6
AlexOlr <63683726+AlexOlr@users.noreply.github.com>
2021-03-14 13:27:15 -0300

    .

--- TestesAulas.c
+++ TestesAulas.c
@@ -5,6 +5,7 @@ int main(){
     int * pi; // ponteiro de inteiros
     float * pf; // ponteiro de float
     char * pc; // ponteiro de char
+    int * vetor; // ponteiro de inteiros
 
     pi = malloc(4); // Alocando um espaço (região de alocação dinamica) para armazenar um inteiro e devolvendo o endereço para PI
     pi = malloc(sizeof(int)); // Alocando um espaço (região de alocação dinamica) para armazenar um inteiro e devolvendo o endereço para PI
@@ -18,6 +19,12 @@ int main(){
     pi = (int *) malloc(sizeof(int));
     pf = (float *) malloc(sizeof(float));
     pc = (char *) malloc(sizeof(char));
+
+    // Para fazer um vetor de tamanho muito grande, por exemplo 1 milhão, precisamos usar o MALLOC pois ele vai usar a parte
+    // de alocação dinamica da memoria RAM, diferente de simplesmente declarar INT VETOR[1000000], que iria dar erro
+    // por tentar alocar na parte de variaveis estaticas, onde não há espaço suficiente
+    // malloc de vetor de 1 milhão de posições:
+    v = (int *) malloc(1000000 * sizeof(int));
 
     return 0;
 }
