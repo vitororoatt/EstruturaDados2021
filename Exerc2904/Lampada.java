6d7f89bc4d395f140a75df34394b289b780b49dc
AlexOlr <63683726+AlexOlr@users.noreply.github.com>
2021-04-25 09:40:48 -0300

    Create Lampada.java

---
+++ Exerc2904/Lampada.java
@@ -0,0 +1,38 @@
+public class Lampada() {
+    private String tipo;
+    private int tensao;
+    private int potencia;
+    private int status;
+    
+    // getter/setter tipo
+    public String getTipo() {
+		return tipo;
+	}
+    public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+    // getter/setter tensao
+    public int getTensao() {
+		return tensao;
+	}
+    public void setTensao(int tensao) {
+		this.tensao = tensao;
+	}
+
+    // getter/setter tensao
+    public int getPotencia() {
+		return potencia;
+	}
+    public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+    // getter/setter tensao
+    public int getStatus() {
+		return status;
+	}
+    public void setStatus(int status) {
+		this.status = status;
+	}
+}
