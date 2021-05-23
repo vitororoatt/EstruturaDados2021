23524e34079c3fd6a8176805c271c550a2a5f588
AlexOlr <63683726+AlexOlr@users.noreply.github.com>
2021-05-23 15:12:40 -0300

    Update Pilhas.java

--- ExePilhas/src/Pilhas.java
+++ ExePilhas/src/Pilhas.java
@@ -1,3 +1,65 @@
-public class Pilhas {
 
+public class Pilhas {
+	private int dados[];
+	private int topo;
+	private int capacidade;
+	
+	public Pilhas () {
+		this(10);
+	}	
+	public Pilhas (int capacidade) {
+		this.dados = new int[capacidade];
+		this.topo = 0;
+		this.capacidade = capacidade;
+	}
+
+    public void empilhaForte() {
+        for(int i = 0; i < this.capacidade; i++){
+            if(this.dados[i] > this.dados[i+1]){
+                
+            }
+        }
+
+    }
+
+	public boolean estaCheia() {
+		return this.topo == this.capacidade;
+		}
+	public boolean estaVazia() {
+		return this.topo == 0;
+	}
+	public void push (int i) {
+		this.dados[this.topo++] = i;
+	}
+	public int pull () {
+		return this.dados[--topo];
+		//this.topo --;
+		//return this.dados[this.topo];
+	}
+	public int tamanho() {
+		return this.topo;
+	}
+	//consulta do elemento do topo da pilha
+	public int consultaTopo() {
+		return this.dados[this.topo-1];
+	}
+	@Override
+	public String toString() {
+		String sPilha = "Pilha: ";
+		if (this.estaVazia()) {
+			sPilha = sPilha + "est� vazia";
+		}
+		else {
+			sPilha = sPilha + "Capacidade: " + this.capacidade + " Tamanho: " + this.tamanho() + "\n";
+			for( int i=this.topo-1; i>=0; i--) {
+				sPilha = sPilha + this.dados[i]+ " ";
+			}
+		}
+		sPilha = sPilha + "\n";
+		return sPilha;
+	}
+	
 }
+
+
+
