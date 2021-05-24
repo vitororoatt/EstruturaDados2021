dd9b265c3a3f6612fa036ca35905647c7fc23c77
AlexOlr <63683726+AlexOlr@users.noreply.github.com>
2021-05-23 21:49:24 -0300

    Upload Recursao

--- ExeRecursao/src/Recursao.java
+++ ExeRecursao/src/Recursao.java
@@ -1,17 +1,21 @@
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Recursao {
 	// exercicio 1
 	public static int somaDosNumeros (int n){
-		if(n == 0)
+		if(n == 0){
 			return 0;
+		}
 		return n + somaDosNumeros(n-1);
 	}
 
 	// exercicio 2
 	public static int numPares (int n){
-        if(n == 0) // se o número for 0
+        if(n == 0){ // se o número for 0
             return 0; // retorna 0
+		}
         if(n % 2 == 0){ // se o número for divisivel por 2
             System.out.println(n); // imprime ele
         }
@@ -19,6 +23,15 @@ public class Recursao {
     }
 
 	// exercicio 3
+	public static int tribonacci (int n){
+		if(n == 0 || n == 1){
+			return 0;
+		}
+		if(n == 2){
+			return 1;
+		}
+		return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+	}
 
 	public static void main(String[] args) {
 		Random gerado = new Random();
@@ -29,6 +42,9 @@ public class Recursao {
 		System.out.println("Teste dos numeros pares, de 0 a N");
 		System.out.println(numPares(gerado.nextInt(10))); // n = random
 		
+		System.out.println("Teste da funcao Tribonacci");
+		int numT = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero Tribonacci que quer imprimir"));
+		System.out.println(tribonacci(numT));
 	}
 
 }
